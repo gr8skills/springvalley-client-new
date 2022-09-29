62d8ec0b05eae2b1df4d7fdd67e97fc4328ef3da
= <=>
2022-09-29 11:12:42 +0100

    another commit

--- js/main.js
+++ js/main.js
@@ -71,6 +71,23 @@ $(document).ready(function(){
   })
 
 
+  document.addEventListener('click', (e) => {
+      const isDropdownButton = e.target.matches("[data-dropdown-btn]")
+      if(!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+      let currentDropdown
+      if(isDropdownButton){
+        // alert("clickedd")
+          currentDropdown = e.target.closest('[data-dropdown]')
+          currentDropdown.classList.toggle('show')
+          
+      }
+
+      document.querySelectorAll("[data-dropdown].show").forEach(dropdown => {
+          if(dropdown === currentDropdown) return
+          dropdown.classList.remove('show')
+      })
+  })
 
 
   $('.owl-testimonial').owlCarousel({
